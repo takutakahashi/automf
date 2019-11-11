90a90e39610206f601de14b0943d774edf5507d4
takutaka <takutaka@pepabo.com>
2019-11-11 13:22:22 +0900

    add cron

---
+++ cron.py
@@ -0,0 +1,14 @@
+# coding: UTF-8
+from crontab import CronTab
+
+if __name__ == '__main__':
+    tasks = ['b_list', 'b_list']
+    cron = CronTab()
+    i = 0
+    for task in tasks:
+        job = cron.new(command="python3 mf.py {}".format(task))
+        job.setall("{} 13 * * *".format(i))
+        i += 1
+    cron.write("./crontab")
+    for result in cron.run_scheduler():
+        print(result)
